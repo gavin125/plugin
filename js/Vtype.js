9edf38c2ca6e9550650d84ddb5d1753cfbbe7a17
gavin <278033412@qq.com>
2016-05-16 10:03:48 +0800

    添加Jquery版本vtype.js

--- js/Vtype.js
+++ js/Vtype.js
@@ -1,83 +1,132 @@
 /*! Vtype 1.0 for migang */
 /*
- *当失焦时进行表单验证，需要给表单配置Vtype属性如下：
- *   用户名：Vtype="username"
- *     密码：Vtype="password"
- * 重复密码：Vtype="again_pw"
- * 手机号码：Vtype="moblie"
- *     邮箱：Vtype="email"
- *
+ * 获得焦点时出现提示，失去焦点时判断结果
  */
 
 var Vtype={
-    getTxt:function(a,b){a.next("i").addClass("error").text(b)},
-    getRight:function(a){a.next("i").removeClass("error").text("√")},
-    err_txt:{
-        null:"不能为空",
-        form:"格式不正确",
-        again:"两次密码不一致"
-    },
-    userName:function(a){//用户名规则：5-12位数字或字母，不含符号。
-        a.focusout(function(){
-            if(a.val()==""){Vtype.getTxt(a,Vtype.err_txt.null)}//为空
-            else if(a.val().match(/^[a-zA-Z0-9]{5,12}$/)==null){Vtype.getTxt(a,Vtype.err_txt.form)}//格式不正确
-            else{Vtype.getRight(a)}
-        }).focusin(function(){
-            a.keyup(function(){if(a.next("i").html()!=""){Vtype.getTxt(a,"")}});//聚焦后输入时，解除验证
-        })
-    },
-    passWord:function(a){
-        a.focusout(function(){//密码规则字母开头，6-18位（含字母数字下划线）字符
-            if(a.val()==""){Vtype.getTxt(a,Vtype.err_txt.null)}
-            else if(a.val().match(/^[a-zA-Z]\w{5,17}$/)==null){Vtype.getTxt(a,Vtype.err_txt.form)}
-            else{Vtype.getRight(a)}
-        }).focusin(function(){
-            a.keyup(function(){if(a.next("i").html()!=""){Vtype.getTxt(a,"")}});//聚焦后输入时，解除验证
-        })
-    },
-    againPw:function(a,b){
-        a.focusout(function(){//先保证密码合规正常，在验证重复密码
-            if(b.val().match(/^\S{6,18}$/)!=null){
-                if(a.val()==""){Vtype.getTxt(a,Vtype.err_txt.null)}
-                else if(a.val()!= b.val()){Vtype.getTxt(a,Vtype.err_txt.again)}
-                else{Vtype.getRight(a)}
+    //用户名
+    userName:function(a){
+        var msg=a.parent().siblings(".msg");
+        a.focusin(function(){
+            msg.html("<span></span><i></i>以字母、数字、下划线组成的5-15字符");
+        }).focusout(function(){
+            var re=/^\w{5,15}$/i;//5-15位数字或字母下划线
+            if(a.val()==""){
+                msg.html("<span></span><i class='error'></i>请输入用户名")
+            }else if(!re.test(this.value)) {
+                msg.html("<span></span><i class='error'></i>用户名格式不正确")
+            }else{
+                msg.html("<span></span><i class='ok'></i>ok")
             }
-        }).focusin(function(){
-            a.keyup(function(){if(a.next("i").html()!=""){Vtype.getTxt(a,"")}});//聚焦后输入时，解除验证
         })
     },
-    mobile:function(a){
-        a.focusout(function(){//手机号码规则13/15/17/18开头的11位数字
-            if(a.val()==""){Vtype.getTxt(a,Vtype.err_txt.null)}
-            else if(a.val().match(/^[1][3578]\d{9}$/)==null){Vtype.getTxt(a,Vtype.err_txt.form)}
-            else{Vtype.getRight(a)}
-        }).focusin(function(){
-            a.keyup(function(){if(a.next("i").html()!=""){Vtype.getTxt(a,"")}});//聚焦后输入时，解除验证
+    //密码
+    passWord:function(a,b){
+        var msg=a.parent().siblings(".msg");
+        a.focusin(function(){
+            msg.html("<span></span><i></i>6-18字符，请牢记您的密码");
+        }).keyup(function(){
+            if(a.val().length>=8 && a.val().length<10){
+                a.parent().siblings(".pwlevel").find("em").eq(1).addClass("active").next().removeClass("active")
+            }else if(a.val().length>=10){
+                a.parent().siblings(".pwlevel").find("em").eq(2).addClass("active")
+            }else{
+                a.parent().siblings(".pwlevel").find("em").eq(1).removeClass("active")
+            }
+            if(a.val().length>=6){b.removeAttr("disabled");}
+        }).focusout(function(){
+            var re=/^[a-zA-Z]\w{5,17}$/i;
+            if(a.val()==""){
+                msg.html("<span></span><i class='error'></i>请输入密码")
+            }else if(a.val().length<6) {
+                msg.html("<span></span><i class='error'></i>密码太短，不太安全哦")
+            }else if(a.val().length>18) {
+                msg.html("<span></span><i class='error'></i>密码太短，不利于记忆哦")
+            }else{
+                msg.html("<span></span><i class='ok'></i>ok")
+            }
+        })
+    },
+    //重复密码
+    againPw:function(a,b){
+        var msg=b.parent().siblings(".msg");
+        b.focusin(function(){
+            msg.html("<span></span><i></i>再次输入密码");
+        }).focusout(function(){
+            if(b.val()==""){
+                msg.html("<span></span><i class='error'></i>请再次输入密码")
+            }else if(b.val()!= a.val()) {
+                msg.html("<span></span><i class='error'></i>两次密码不一致")
+            }else{
+                msg.html("<span></span><i class='ok'></i>ok")
+            }
+        })
+    },
+    phoneCode:function(a){
+        var msg=a.parent().siblings(".msg");
+        var re=/^[1][3578]\d{9}$/i;
+        a.focusin(function(){
+            msg.html("<span></span><i></i>请输入手机号码");
+        }).focusout(function(){
+            if(a.val()==""){
+                msg.html("<span></span><i class='error'></i>请输入手机号码")
+            }else if(!re.test(a.val())){
+                msg.html("<span></span><i class='error'></i>手机号格式不正确")
+            }else{
+                msg.html("<span></span><i class='ok'></i>ok")
+            }
         })
     },
     eMail:function(a){
-        a.focusout(function(){//邮箱以字母数字或下划线中划线点号开头，加@，在字母数字中划线，加.,在2到6位域名后缀
-            if(a.val()==""){Vtype.getTxt(a,Vtype.err_txt.null)}
-            else if(a.val().match( /^([0-9]{15})+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,6})+$/)==null){Vtype.getTxt(a,Vtype.err_txt.form)}
-            else{Vtype.getRight(a)}
-        }).focusin(function(){
-            a.keyup(function(){if(a.next("i").html()!=""){Vtype.getTxt(a,"")}});//聚焦后输入时，解除验证
+        var msg=a.parent().siblings(".msg");
+        var re=/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/i;
+        a.focusin(function(){
+            msg.html("<span></span><i></i>您填写的邮箱将作为安全邮箱");
+        }).focusout(function(){
+            if(a.val()==""){
+                msg.html("<span></span><i class='error'></i>请输入邮箱")
+            }else if(!re.test(a.val())){
+                msg.html("<span></span><i class='error'></i>邮箱格式不正确")
+            }else{
+                msg.html("<span></span><i class='ok'></i>ok")
+            }
         })
     },
     idCard:function(a){
-        a.focusout(function(){//邮箱以字母数字或下划线中划线点号开头，加@，在字母数字中划线，加.,在2到6位域名后缀
-            if(a.val()==""){Vtype.getTxt(a,Vtype.err_txt.null)}
-            else if(a.val().match( /^\d{15}|\d{18}$/)==null){Vtype.getTxt(a,Vtype.err_txt.form)}
-            else{Vtype.getRight(a)}
-        }).focusin(function(){
-            a.keyup(function(){if(a.next("i").html()!=""){Vtype.getTxt(a,"")}});//聚焦后输入时，解除验证
+        var msg=a.parent().siblings(".msg");
+        var re=/^(\d{18,18}|\d{15,15}|\d{17,17}x)$/i;
+        a.focusin(function(){
+            msg.html("<span></span><i></i>身份证号码将进行实名认证");
+        }).focusout(function(){
+            if(a.val()==""){
+                msg.html("<span></span><i class='error'></i>请输入身份证号码")
+            }else if(!re.test(a.val())){
+                msg.html("<span></span><i class='error'></i>身份证号码格式不正确")
+            }else{
+                msg.html("<span></span><i class='ok'></i>ok")
+            }
+        })
+    },
+    bankCard:function(a){
+        var msg=a.parent().siblings(".msg");
+        var re=/^(\d{16,16}|\d{19,19})$/i;
+        a.focusin(function(){
+            msg.html("<span></span><i></i>请填写常用银行卡");
+        }).focusout(function(){
+            if(a.val()==""){
+                msg.html("<span></span><i class='error'></i>请输入银行卡号")
+            }else if(!re.test(a.val())){
+                msg.html("<span></span><i class='error'></i>银行卡号格式不正确")
+            }else{
+                msg.html("<span></span><i class='ok'></i>ok")
+            }
         })
     }
-
 };
-Vtype.userName($("input[Vtype=username]"));
-Vtype.passWord($("input[Vtype=password]"));
-Vtype.againPw($("input[Vtype=again_pw]"),$("input[Vtype=password]"));
-Vtype.mobile($("input[Vtype=mobile]"));
-Vtype.eMail($("input[Vtype=email]"));
-Vtype.idCard($("input[Vtype=idcard]"));
+Vtype.userName($("#uName"));
+Vtype.passWord($("#pWord"),$("#againPw"));
+Vtype.againPw($("#pWord"),$("#againPw"));
+Vtype.phoneCode($("#pCode"));
+Vtype.eMail($("#eMail"));
+Vtype.idCard($("#iCard"));
+Vtype.bankCard($("#bCard"));
--- js/Vtypejs.js
+++ js/Vtypejs.js
@@ -13,7 +13,7 @@ window.onload=function(){
     };
     uName.onblur=function(){
         var msg=username.getElementsByTagName("p")[0];
-        var re=/^\w{5,8}$/i;
+        var re=/^\w{5,15}$/i;
         if(this.value==""){//为空
             msg.innerHTML="<span></span><i class='error'></i>请输入用户名"
         }else if(!re.test(this.value)){//格式不正确
@@ -150,40 +150,3 @@ window.onload=function(){
     };
 };
 
-var XMLHttpReq;
-function createXMLHttpRequest() {
-    try {
-        XMLHttpReq = new ActiveXObject("Msxml2.XMLHTTP");//IE高版本创建XMLHTTP
-    }
-    catch(E) {
-        try {
-            XMLHttpReq = new ActiveXObject("Microsoft.XMLHTTP");//IE低版本创建XMLHTTP
-        }
-        catch(E) {
-            XMLHttpReq = new XMLHttpRequest();//兼容非IE浏览器，直接创建XMLHTTP对象
-        }
-    }
-}
-function sendAjaxRequest(url) {
-    createXMLHttpRequest();                                //创建XMLHttpRequest对象
-    XMLHttpReq.open("post", url, true);
-    XMLHttpReq.onreadystatechange = processResponse; //指定响应函数
-    XMLHttpReq.send(null);
-}
-//回调函数
-function processResponse() {
-    if (XMLHttpReq.readyState == 4) {
-        if (XMLHttpReq.status == 200) {
-            var text = XMLHttpReq.responseText;
-            console.log(text);
-            /**
-             *实现回调
-             */
-            text = window.decodeURI(text);
-            console.log(text);
-
-
-        }
-    }
-
-}
